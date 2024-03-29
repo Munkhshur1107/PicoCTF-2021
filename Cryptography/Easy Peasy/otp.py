615bda8e1afb5b4e950dee9c3bc0a0c84db5666b
Munkhshur1107 <165356298+Munkhshur1107@users.noreply.github.com>
2024-03-29 10:26:58 +0530

    Update otp.py

--- Cryptography/Easy Peasy/otp.py
+++ Cryptography/Easy Peasy/otp.py
@@ -2,7 +2,7 @@
 import os.path
 
 KEY_FILE = "key"
-KEY_LEN = 50000
+KEY_LEN = 40000
 FLAG_FILE = "flag"
 
 
@@ -32,8 +32,8 @@ def encrypt(key_location):
 	kf = open(KEY_FILE, "rb").read()
 
 	if stop >= KEY_LEN:
-		stop = stop % KEY_LEN
-		key = kf[start:] + kf[:stop]
+	stop = stop % KEY_LEN
+	key = kf[start:] + kf[:stop]
 	else:
 		key = kf[start:stop]
 	key_location = stop
@@ -49,3 +49,5 @@ print("******************Welcome to our OTP implementation!******************")
 c = startup(0)
 while c >= 0:
 	c = encrypt(c)
+
+pwn template --host mercury.picoctf.net --port 20266 otp.py
